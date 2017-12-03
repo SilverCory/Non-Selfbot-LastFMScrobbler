27269e68d44c301e0aa5caf9babae77513aa1933
Cory Redmond <ace@ac3-servers.eu>
2017-12-03 19:32:45 +0000

    Don't use pointer

--- scrobbler/init.go
+++ scrobbler/init.go
@@ -1,22 +1,22 @@
 package scrobbler
 
-var scrobblers = make(map[string]*ScrobbleSource)
+var scrobblers = make(map[string]ScrobbleSource)
 
-func RegisterSource(name string, source *ScrobbleSource) {
+func RegisterSource(name string, source ScrobbleSource) {
 	scrobblers[name] = source
 }
 
-func GetScrobbler(name string) (sco *ScrobbleSource, ok bool) {
+func GetScrobbler(name string) (sco ScrobbleSource, ok bool) {
 	sco, ok = scrobblers[name]
 	return
 }
 
-func GetAllScrobblers() map[string]*ScrobbleSource {
+func GetAllScrobblers() map[string]ScrobbleSource {
 	return scrobblers
 }
 
-func getOrganisedScrobblers(priorities map[string]int) map[int]*ScrobbleSource {
-	var ret = make(map[int]*ScrobbleSource)
+func getOrganisedScrobblers(priorities map[string]int) map[int]ScrobbleSource {
+	var ret = make(map[int]ScrobbleSource)
 	for k, v := range scrobblers {
 		ret[priorities[k]] = v
 	}
